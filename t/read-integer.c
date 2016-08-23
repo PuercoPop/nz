da7b5cd7bb5740851b9a7e50d8258db69877dca4
Javier Olaechea <pirata@gmail.com>
2016-08-23 16:27:18 -0500

    style change

--- t/read-integer.c
+++ t/read-integer.c
@@ -6,7 +6,8 @@
 int
 main(int argc, char **argv) {
   FILE *f = fopen("t/integer.nz", "r");
-  if (f == NULL) perror("Unable to open file: integer.nz");
+  if (f == NULL)
+    perror("Unable to open file: integer.nz");
 
   int i = nz_read_integer(f);
 
