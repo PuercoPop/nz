d69970987269c85c6d70090c1840f3d58d4810bb
Javier Olaechea <pirata@gmail.com>
2017-01-19 04:18:10 -0500

    Remove initial WIP

--- config.mk
+++
@@ -1,8 +0,0 @@
-CC = clang
-INCS = -I. -I/usr/include -I/usr/include/freetype2
-LIBS = -L/usr/lib -lc -lX11 -lfontconfig -lXft
-CFLAGS = -Wall -Wpedantic -g -std=c99 ${INCS}
-LDFLAGS = ${LIBS}
-
-CFLAGS +=  `pkg-config --cflags guile-2.0`
-LIBS +=  `pkg-config --libs guile-2.0`
--- lisp.h
+++
@@ -1,34 +0,0 @@
-#ifndef LISP_H
-#define LISP_H
-
-#include <stdio.h>
-
-
-// Types.h
-typedef struct LispObj {
-} LispObj;
-
-enum {
-  Symbol, NZ_Int;
-}
-#define MAX_SYMBOL_LENGTH 100
-
-typedef struct Symbol {
-typedef struct NZ_Symbol {
-  char* name;
-} NZ_Symbol;
-
-
-
-// Symbols.h
-NZ_Symbol make_symbol(char *);
-
-
-// Read.h
-LispObj* nz_read(FILE*);
-int read_symbol(FILE*);
-int read_number(FILE*);
-char* nz_read_symbol(FILE*);
-int nz_read_integer(FILE*);
-
-#endif
