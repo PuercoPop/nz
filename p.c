94704242a1feb635f20ee3534b6583c05d2c9404
Javier Olaechea <pirata@gmail.com>
2018-05-06 20:08:02 -0500

    'P' command done
    It prints the contents of a file to standard out.

---
+++ p.c
@@ -0,0 +1,46 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+void
+die(char *msg, ...)
+{
+  va_list ap;
+
+  va_start(ap, msg);
+  vfprintf(stderr, msg, ap);
+  va_end(ap);
+  exit(1);
+}
+
+int
+main(int argc, char *argv[])
+{
+  if (argc < 2)
+    die("Must supply filename.\n");
+
+  char *fpath = argv[1];
+
+  int fd = open(fpath, O_RDONLY);
+  if (fd < 0)
+    die("Could not open file: %s\n", fpath);
+
+  char buf[BUFSIZ];
+
+  int len;
+  len = read(fd, buf, BUFSIZ);
+  for(len = read(fd, buf, BUFSIZ);
+      len > 0;
+      len = read(fd, buf, BUFSIZ))
+  {
+    write(1, buf, len);
+  }
+
+  close(fd);
+  
+  return EXIT_SUCCESS;
+}
